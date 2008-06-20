7c22930187f4f87614a3646716bf4118b65f2bb6
brosner <brosner@gmail.com>
2008-06-20 13:44:10 -0600

    Added a missing import and fixed a typo.

--- versioning/__init__.py
+++ versioning/__init__.py
@@ -8,10 +8,12 @@ def register(model):
     """
     """
     from django.db.models import signals as model_signals
+    from django.dispatch import dispatcher
+    
     from versioning.signals import pre_save
     
     if model in registry:
         raise AlreadyRegistered
-    registery.append(model)
+    registry.append(model)
     
     dispatcher.connect(pre_save, signal=model_signals.pre_save, sender=model)
