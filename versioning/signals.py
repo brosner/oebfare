b0eede92d8b043484b8386316915085df3a3d5c2
brosner <brosner@gmail.com>
2008-06-20 13:54:13 -0600

    Made the pre_save hook actually do something useful.

--- versioning/signals.py
+++ versioning/signals.py
@@ -1,5 +1,13 @@
 
+from django.contrib.contenttypes.models import ContentType
+
+from versioning.models import Revision
+
 def pre_save(instance, **kwargs):
     """
     """
-    import pdb ; pdb.set_trace()
+    model = kwargs["sender"]
+    original = model._default_manager.get(pk=instance.pk)
+    rev = Revision(object_pk=instance.pk,
+                   content_type=ContentType.objects.get_for_model(model))
+    rev.save()
